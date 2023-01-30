c188c339cc8bca7cdd44f3d491dc654c73e5387d
Alexander Korobov <dktwho@mail.ru>
2023-01-30 13:27:43 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
 import './App.css';
+import { initNotes } from './data';
 
 function App() {
+  const [notes, setNotes] = useState(initNotes)
+  const [obj, setObj] = useState(getInitObj())
+  const [editId, setEditId] = useState(null)
+
+  function getInitObj() {
+    return {
+      id: nanoid(),
+      prop1: '',
+      prop2: '',
+      prop3: '',
+    }
+  }
+
+  const result = notes.map((note) => {
+    return <p key={note.id}>
+      <span>{note.prop1}</span>
+      <span>{note.prop2}</span>
+      <span>{note.prop3}</span>
+      <button onClick={() => setEditId(note.id)}>edit</button>
+    </p>
+  })
+
+  function getValue(prop) {
+    if(editId) {
+      return notes.reduce((res, note) => note.id === editId ? note[prop] : res, '')
+    } else {
+      return obj[prop]
+    }
+  }
+
+  function changeItem(prop, event) {
+    if(editId) {
+      setNotes(notes.map((note) => note.id === editId ? {...note, [prop]: event.target.value} : note))
+    } else {
+      setObj({...obj, [prop]: event.target.value})
+    }
+  }
+
+  function saveItem() {
+    if(editId) {
+      setEditId(null)
+    } else {
+      setNotes([...notes, obj])
+      setObj(getInitObj())
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {result}
+      <input type="text" value={getValue('prop1')}  onChange={(e) => changeItem('prop1', e,)} /> <br />
+      <input type="text" value={getValue('prop2')}  onChange={(e) => changeItem('prop2', e,)} /> <br /> 
+      <input type="text" value={getValue('prop3')}  onChange={(e) => changeItem('prop3', e,)} /> <br />
+      <button onClick={saveItem}>save</button>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,45 @@
+const initNotes = [
+	{
+		id: 'GYi9G_uC4gBF1e2SixDvu',
+		prop1: 'value11',
+		prop2: 'value12',
+		prop3: 'value13',
+	},
+	{
+		id: 'IWSpfBPSV3SXgRF87uO74',
+		prop1: 'value21',
+		prop2: 'value22',
+		prop3: 'value23',
+	},
+	{
+		id: 'JAmjRlfQT8rLTm5tG2m1L',
+		prop1: 'value31',
+		prop2: 'value32',
+		prop3: 'value33',
+	},
+];
+
+const users = [
+  {
+    id:1,
+    userName: 'Bill',
+    lastName: 'Robson',
+    age: 23
+  },
+  {
+    id: 2,
+    userName: 'Ted',
+    lastName: 'Hill',
+    age: 35
+  },
+  {
+    id: 3,
+    userName: 'Joe',
+    lastName: 'Larson',
+    age: 45
+  },
+
+
+]
+
+export { initNotes, users }
